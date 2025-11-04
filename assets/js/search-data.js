c34e5a18c70f081a7428ec6e0483306b5ba205d0
rfeng77 <fleurhan77@gmail.com>
2025-11-04 03:38:19 +0000

    Deploying to gh-pages from @ rfeng77/rfeng77.github.io@370dea618493e8d0e98aa672dd3c4835416a67fa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,13 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-passed-the-preliminary-examination-now-i-am-doctoral-candidate",
+          title: 'Passed the Preliminary Examination! Now I am Doctoral Candidate!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
