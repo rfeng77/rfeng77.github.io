8eb23792443647f2cdab83ad3ae280d8661e7d29
rfeng77 <fleurhan77@gmail.com>
2025-11-04 02:26:48 +0000

    Deploying to gh-pages from @ rfeng77/rfeng77.github.io@8c29c6d4585462daf1e4c54df935474301fddc2d 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "rfeng77/fleurfeng.github.io",
+    "data-repo": "rfeng77/rfeng77.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
