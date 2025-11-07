e8eab0d0fcd5a4723b2bd272ec224ec53a971c14
rfeng77 <fleurhan77@gmail.com>
2025-11-07 02:24:13 +0000

    Deploying to gh-pages from @ rfeng77/rfeng77.github.io@92f82996884f33eb0dc5009793e6b2ed44c42a64 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-publications",
           title: "publications",
-          description: "Selected publications and works under review.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
