b4a60c0740914b4c03851c08cceae5380cff4b1f
rfeng77 <fleurhan77@gmail.com>
2025-11-07 04:21:54 +0000

    Deploying to gh-pages from @ rfeng77/rfeng77.github.io@05d53702013a368b8af5bf7c44c566f8ff9c9bef 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
-          title: "CV",
-          description: "Curriculum Vitae",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "nav-blog",
+  },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
