12679cfa2aa9684afe17c3043916509176882804
rfeng77 <fleurhan77@gmail.com>
2025-11-07 04:31:42 +0000

    Deploying to gh-pages from @ rfeng77/rfeng77.github.io@b2add854da318752e7fbd60be401c4d738ed0140 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "Academic CV and publication record.",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
