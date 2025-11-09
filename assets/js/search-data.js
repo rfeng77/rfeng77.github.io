ec6bd202bd50dbdddfe792138505c2685302df1e
rfeng77 <fleurhan77@gmail.com>
2025-11-09 03:26:03 +0000

    Deploying to gh-pages from @ rfeng77/rfeng77.github.io@5d75456b43cf00ba167b0a7c4ad1a31aa9eab94d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -426,9 +426,9 @@ ninja.data = [{
           description: "Integrating machine learning and structural modeling to uncover how mindfulness enhances well-being through flexibility and reduced rumination.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-mind-body-interventions-and-neural-mechanisms-of-healthy-aging",
+          title: 'Mind–Body Interventions and Neural Mechanisms of Healthy Aging',
+          description: "A systematic review and meta-analysis of neuroimaging evidence on meditation, Tai Chi, and yoga in older adults, revealing brain–behavior coupling that supports cognitive and emotional resilience.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
